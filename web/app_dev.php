d70fba8a549c9488d1b53a0fef21d9ac040a4c80
IlhamTahir <yantaq@bilig.biz>
2017-06-02 10:35:36 +0800

    add new AppBundle

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,7 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
-
+date_default_timezone_set('Asia/Shanghai');
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
