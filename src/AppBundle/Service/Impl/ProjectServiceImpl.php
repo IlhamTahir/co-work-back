d3f7f5677675a35fa259ddb36ec0be119f6e9ef2
IlhamTahir <yantaq@bilig.biz>
2017-08-09 18:54:18 +0800

    add Project Service

---
+++ src/AppBundle/Service/Impl/ProjectServiceImpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilham
+ * Date: 8/9/17
+ * Time: 6:37 PM
+ */
+
+namespace AppBundle\Service\Impl;
+
+use AppBundle\Entity\Project;
+use AppBundle\Service\ProjectService;
+
+class ProjectServiceImpl implements ProjectService
+{
+    public function getProject($id)
+    {
+        // TODO: Implement getProject() method.
+    }
+
+}
---
+++ src/AppBundle/Service/ProjectService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilham
+ * Date: 8/9/17
+ * Time: 4:28 PM
+ */
+
+namespace AppBundle\Service;
+
+interface ProjectService
+{
+    public function getProject($id);
+
+}
